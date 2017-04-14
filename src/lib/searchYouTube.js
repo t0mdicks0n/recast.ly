e95d7c0fc35c676e051f409850cafc457e505403
Hack Reactor Student <student@hackreactor.com>
2017-04-13 19:57:47 -0700

    Added functionality to call API.

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,52 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+	var appServer = 'https://www.googleapis.com/youtube/v3/search'
+
+	// $.ajax({
+	// 		type: 'GET',
+	//     url: appServer,
+	//     dataType: 'json',
+	//     data: {maxResults: 5},
+	//     success: function (data) {
+	//     	console.log(data);
+	//     },
+	//     error: function (data) {
+	//       console.error('youtube: Failed to send message', data);
+	//     }
+	//   });
+
+	var params = {
+		part: 'snippet',
+		key: options.key,
+		max: options.max,
+		q: options.query,
+		type: 'video',
+		videoEmbeddable: true,
+	};
+
+	$.getJSON(appServer, params, function (data) {
+		data.items.forEach(function(element, index, array) {
+			callback(element);
+		});
+	});
 };
 
+searchYouTube({key:'AIzaSyDQpUvp8t-cGuBAvNUrRVYlkVYZfCS5KT0', max:5, query:'Tom'}, function(item) {
+	console.log(item);
+});
+
+// function getRequest(searchTerm) {
+//     var params = {
+//         part: 'snippet',
+//         key: 'AIzaSyDQpUvp8t-cGuBAvNUrRVYlkVYZfCS5KT0',
+//         max:
+//         q: searchTerm
+//     };
+  
+//     $.getJSON('https://www.googleapis.com/youtube/v3/search', params, function (searchTerm) {
+//         console.log(searchTerm);
+//     });
+// }
+
 window.searchYouTube = searchYouTube;
+
+
